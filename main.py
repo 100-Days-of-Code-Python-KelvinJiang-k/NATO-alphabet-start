2cce759d2c748c9ce39bc05c74810ac1adad05ae
KelvinJiang <kelvinkxjiang@gmail.com>
2022-08-23 12:52:09 -0700

    added error handling and cleaned up file

--- main.py
+++ main.py
@@ -1,35 +1,25 @@
+import pandas
+
 student_dict = {
     "student": ["Angela", "James", "Lily"],
     "score": [56, 76, 98]
 }
 
-# Looping through dictionaries:
-for (key, value) in student_dict.items():
-    # Access key and value
-    pass
-
-import pandas
-
 student_data_frame = pandas.DataFrame(student_dict)
-
-# Loop through rows of a data frame
-for (index, row) in student_data_frame.iterrows():
-    # Access index and row
-    # Access row.student or row.score
-    pass
-
-# Keyword Method with iterrows()
-# {new_key:new_value for (index, row) in df.iterrows()}
-
-{"A": "Alfa", "B": "Bravo"}
 
 nato_alphabet = pandas.read_csv("nato_phonetic_alphabet.csv")
 nato_name = {row.letter: row.code for (index, row) in nato_alphabet.iterrows()}
 
-# nato_name = {letter: nato_alphabet[nato_alphabet["letter"] == letter].code for letter in name}
-
 name = input("What is your name?  ").upper()
-print(name)
 
-nato_wordlist = [nato_name[letter] for letter in name]
-print(nato_wordlist)
+
+def generate_phonetic():
+    try:
+        nato_wordlist = [nato_name[letter] for letter in name]
+    except KeyError:
+        print("Invalid name input; only letters are allowed.")
+        generate_phonetic()
+    else:
+        print(nato_wordlist)
+
+generate_phonetic()
